dbc5db5ae63617025a3e8bc83117493dc6864d6b
Ose <37250900+osenaattori@users.noreply.github.com>
2021-12-24 01:10:46 +0200

    Update work.js

--- components/work.js
+++ components/work.js
@@ -5,7 +5,7 @@ import { ChevronRightIcon } from '@chakra-ui/icons'
 export const Title = ({ children }) => (
   <Box>
     <NextLink href="/works">
-      <Link>Works</Link>
+      <Link>Työt</Link>
     </NextLink>
     <span>
       {' '}
