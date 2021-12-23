a81d94fa29960eabe501077dd20b94127fa62f5c
Ose <37250900+osenaattori@users.noreply.github.com>
2021-12-24 00:19:51 +0200

    gg

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,9 @@ import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+import thumbYouTube from '../public/images/links/youtube.png'
+import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
 
 
 const Home = () => (
