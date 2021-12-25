1b3680f457a79bf5733ecac6ac16939a5bd19868
Ose <37250900+osenaattori@users.noreply.github.com>
2021-12-25 16:15:45 +0200

    Update works.js

--- pages/works.js
+++ pages/works.js
@@ -1,7 +1,6 @@
 import { Container, Heading, SimpleGrid,  } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
-import Section from '../components/section'
-import { WorkGridItem } from '../components/grid-item'
+
 
 
 
